e10687df7d461fe9951aaf9dc22ba01d9791cc65
Martin Brandl <mjibrandl@gmail.com>
2020-09-02 15:20:35 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MVP Docs & Learn Champion Chrome extension
+
+This extension adds a 'Copy link address with CreatorID' item to your Chrome on eligible sites. It will also ensure that the CreatorID is added properly to the URL so you don't have to worry about whether the URL already contains a query fragment (`?=`):
+
+![Image of the context menue](./assets/context_menue.png)
---
+++ main.js
@@ -0,0 +1,47 @@
+// Dear MVP fellow, please enter your Creator ID here:
+const CREATOR_ID = "AZ-MVP-5003203";
+const QUERY_KEY = 'WT.mc_id'
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.contextMenus.removeAll();
+    chrome.contextMenus.create({
+        title: 'Copy link address with CreatorID',
+        id: 'docslearnchampion',
+        documentUrlPatterns: [
+            "http://social.technet.microsoft.com/*",
+            "https://docs.microsoft.com/*",
+            "https://azure.microsoft.com/*",
+            "https://techcommunity.microsoft.com/*",
+            "https://social.msdn.microsoft.com/*",
+            "https://devblogs.microsoft.com/*",
+            "https://developer.microsoft.com/*",
+            "https://channel9.msdn.com/*",
+            "https://gallery.technet.microsoft.com/*",
+            "https://cloudblogs.microsoft.com/*",
+            "https://technet.microsoft.com/*",
+            "https://docs.azure.cn/*",
+            "https://www.azure.cn/*",
+            "https://msdn.microsoft.com/*",
+            "https://blogs.msdn.microsoft.com/*",
+            "https://blogs.technet.microsoft.com/*",
+            "https://microsoft.com/handsonlabs/*"
+        ],
+        contexts: ['link']
+    });
+});
+
+chrome.contextMenus.onClicked.addListener(function (itemData) {
+    var url = new URL(itemData.linkUrl)
+    url.searchParams.append(QUERY_KEY, CREATOR_ID)
+    copyTextToClipboard(url.href)
+});
+
+function copyTextToClipboard(text) {
+    var copyFrom = document.createElement("textarea");
+    copyFrom.textContent = text;
+    document.body.appendChild(copyFrom);
+    copyFrom.select();
+    document.execCommand('copy');
+    copyFrom.blur();
+    document.body.removeChild(copyFrom);
+}
