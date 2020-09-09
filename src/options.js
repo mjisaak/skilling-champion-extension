b98c98d8d33618c955620c5a5273ff75b6fca641
Martin Brandl <mjibrandl@gmail.com>
2020-09-09 23:02:50 +0200

    initial draft

--- src/options.js
+++ src/options.js
@@ -1,26 +1,82 @@
+const btnAdd = document.querySelector('#btnAdd');
+const btnRemove = document.querySelector('#btnRemove');
+const sb = document.querySelector('#list');
+const name = document.querySelector('#name');
+
+btnAdd.onclick = (e) => {
+    e.preventDefault();
+
+    // Ensure the CreatorID doesn't contain whitespaces and remove the query key if present
+    let creatorId = name.value.replace('?WT.mc_id=', '').trim()
+
+
+
+    const option = new Option(creatorId, creatorId);
+    sb.add(option, undefined);
+
+    chrome.storage.sync.set({
+        list: Object.keys(sb.options).map(f => sb.options[f].value)
+    }, function () { });
+
+    // reset the value of the input
+    name.value = '';
+    name.focus();
+};
+
+btnRemove.onclick = (e) => {
+    e.preventDefault();
+
+    // save the selected option
+    let selected = [];
+
+    for (let i = 0; i < sb.options.length; i++) {
+        selected[i] = sb.options[i].selected;
+    }
+
+    // remove all selected option
+    let index = sb.options.length;
+    while (index--) {
+        if (selected[index]) {
+            sb.remove(index);
+        }
+    }
+};
+
 function save_options() {
     var creatorId = document.getElementById('creatorId').value.replace('?WT.mc_id=', '');
     document.getElementById('creatorId').value = creatorId;
 
-    chrome.storage.sync.set({
-        creatorId: creatorId
-    }, function () {
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function () {
-            status.textContent = '';
-        }, 1000);
-    });
+
 }
 
 function restore_options() {
 
     chrome.storage.sync.get({
-        creatorId: '',
+        list: [],
     }, function (items) {
-        document.getElementById('creatorId').value = items.creatorId;
+
+
+
+
+        console.log("items:", items)
+        items.list.forEach(function (item) {
+            const option = new Option(item, item);
+            sb.add(option, undefined);
+
+            console.log("item:", item)
+
+
+        })
+
+
+
     });
+
+
+
 }
+
+console.log("resoptiontore")
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+// document.getElementById('save').addEventListener('click',
+//     save_options);
