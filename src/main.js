b83423e27ceee7bcd2841d004a7cca454ac94e25
Martin Brandl <mjibrandl@gmail.com>
2020-09-10 16:23:00 +0200

    minor refactoring

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
 const QUERY_KEY = 'WT.mc_id';
 
 chrome.contextMenus.onClicked.addListener(function (itemData) {
-
-    console.log("item", itemData);
     var url = new URL(itemData.linkUrl);
-    console.log("item", itemData);
     url.searchParams.append(QUERY_KEY, itemData.menuItemId);
     copyTextToClipboard(url.href);
-
-
 });
 
 function copyTextToClipboard(text) {
