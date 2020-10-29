c072292f3372028e754428a3898002053b316e03
Martin Brandl <mjibrandl@gmail.com>
2020-10-29 08:58:35 +0100

    initial added webpack

---
+++ src/webpack/webpack.common.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    popup: path.join(__dirname, "../main.ts"),
+    // eventPage: path.join(__dirname, "src/eventPage.ts")
+  },
+  output: {
+    path: path.join(__dirname, "./../../dist/js"),
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.tsx?$/,
+        use: "ts-loader"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  }
+};
